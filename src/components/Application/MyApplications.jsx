44200c77e251c80db337420825e852315991e926
hitanshu0729 <hitanshu0729@gmail.com>
2024-07-19 19:58:39 +0530

    loader

--- src/components/Application/MyApplications.jsx
+++ src/components/Application/MyApplications.jsx
@@ -4,65 +4,57 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import ResumeModal from "./ResumeModal";
+import { RotatingLines } from "react-loader-spinner";
 
 const MyApplications = () => {
   const { user } = useContext(Context);
   const [applications, setApplications] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [resumeImageUrl, setResumeImageUrl] = useState("");
-
+  const [loading, setLoading] = useState(false);
   const { isAuthorized } = useContext(Context);
   const navigateTo = useNavigate();
 
   useEffect(() => {
-    try {
-      if (user && user.role === "Employer") {
-        axios
-          .get(
-            "https://jobjet-backend.onrender.com/api/v1/application/employer/getall",
-            {
-              withCredentials: true,
-            }
-          )
-          .then((res) => {
-            setApplications(res.data.applications);
-          });
-      } else {
-        axios
-          .get(
-            "https://jobjet-backend.onrender.com/api/v1/application/jobseeker/getall",
-            {
-              withCredentials: true,
-            }
-          )
-          .then((res) => {
-            setApplications(res.data.applications);
-          });
-      }
-    } catch (error) {
-      toast.error(error.response.data.message);
-    }
-  }, [isAuthorized]);
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const endpoint =
+          user && user.role === "Employer"
+            ? "https://jobjet-backend.onrender.com/api/v1/application/employer/getall"
+            : "https://jobjet-backend.onrender.com/api/v1/application/jobseeker/getall";
 
-  if (!isAuthorized) {
-    navigateTo("/");
-  }
-
-  const deleteApplication = (id) => {
-    try {
-      axios
-        .delete(
-          `https://jobjet-backend.onrender.com/api/v1/application/delete/${id}`,
-          {
-            withCredentials: true,
-          }
-        )
-        .then((res) => {
-          toast.success(res.data.message);
-          setApplications((prevApplication) =>
-            prevApplication.filter((application) => application._id !== id)
-          );
+        const res = await axios.get(endpoint, {
+          withCredentials: true,
         });
+
+        setApplications(res.data.applications);
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (isAuthorized) {
+      fetchData();
+    } else {
+      navigateTo("/");
+    }
+  }, [isAuthorized, navigateTo, user]);
+
+  const deleteApplication = async (id) => {
+    try {
+      const res = await axios.delete(
+        `https://jobjet-backend.onrender.com/api/v1/application/delete/${id}`,
+        {
+          withCredentials: true,
+        }
+      );
+      toast.success(res.data.message);
+      setApplications((prevApplications) =>
+        prevApplications.filter((application) => application._id !== id)
+      );
     } catch (error) {
       toast.error(error.response.data.message);
     }
@@ -79,123 +71,123 @@ const MyApplications = () => {
 
   return (
     <section className="my_applications page">
-      {user && user.role === "Job Seeker" ? (
-        <div className="container">
-          <h1>My Applications</h1>
-          {applications.length <= 0 ? (
-            <>
-              {" "}
-              <h4>No Applications Found</h4>{" "}
-            </>
-          ) : (
-            applications.map((element) => {
-              return (
-                <JobSeekerCard
-                  element={element}
-                  key={element._id}
-                  deleteApplication={deleteApplication}
-                  openModal={openModal}
-                />
-              );
-            })
-          )}
+      {loading ? (
+        <div
+          className="loading-spinner"
+          style={{ width: "100vw", display: "flex", justifyContent: "center" }}
+        >
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="96"
+            visible={true}
+          />
         </div>
       ) : (
-        <div className="container">
-          <h1>Applications From Job Seekers</h1>
-          {applications.length <= 0 ? (
-            <>
-              <h4>No Applications Found</h4>
-            </>
+        <>
+          {user && user.role === "Job Seeker" ? (
+            <div className="container">
+              <h1>My Applications</h1>
+              {applications.length <= 0 ? (
+                <h4>No Applications Found</h4>
+              ) : (
+                applications.map((element) => (
+                  <JobSeekerCard
+                    element={element}
+                    key={element._id}
+                    deleteApplication={deleteApplication}
+                    openModal={openModal}
+                  />
+                ))
+              )}
+            </div>
           ) : (
-            applications.map((element) => {
-              return (
-                <EmployerCard
-                  element={element}
-                  key={element._id}
-                  openModal={openModal}
-                />
-              );
-            })
+            <div className="container">
+              <h1>Applications From Job Seekers</h1>
+              {applications.length <= 0 ? (
+                <h4>No Applications Found</h4>
+              ) : (
+                applications.map((element) => (
+                  <EmployerCard
+                    element={element}
+                    key={element._id}
+                    openModal={openModal}
+                  />
+                ))
+              )}
+            </div>
           )}
-        </div>
-      )}
-      {modalOpen && (
-        <ResumeModal imageUrl={resumeImageUrl} onClose={closeModal} />
+          {modalOpen && (
+            <ResumeModal imageUrl={resumeImageUrl} onClose={closeModal} />
+          )}
+        </>
       )}
     </section>
   );
 };
 
+const JobSeekerCard = ({ element, deleteApplication, openModal }) => (
+  <div className="job_seeker_card">
+    <div className="detail">
+      <p>
+        <span>Name:</span> {element.name}
+      </p>
+      <p>
+        <span>Email:</span> {element.email}
+      </p>
+      <p>
+        <span>Phone:</span> {element.phone}
+      </p>
+      <p>
+        <span>Address:</span> {element.address}
+      </p>
+      <p>
+        <span>CoverLetter:</span> {element.coverLetter}
+      </p>
+    </div>
+    <div className="resume">
+      <img
+        src={element.resume.url}
+        alt="resume"
+        onClick={() => openModal(element.resume.url)}
+      />
+    </div>
+    <div className="btn_area">
+      <button onClick={() => deleteApplication(element._id)}>
+        Delete Application
+      </button>
+    </div>
+  </div>
+);
+
+const EmployerCard = ({ element, openModal }) => (
+  <div className="job_seeker_card">
+    <div className="detail">
+      <p>
+        <span>Name:</span> {element.name}
+      </p>
+      <p>
+        <span>Email:</span> {element.email}
+      </p>
+      <p>
+        <span>Phone:</span> {element.phone}
+      </p>
+      <p>
+        <span>Address:</span> {element.address}
+      </p>
+      <p>
+        <span>CoverLetter:</span> {element.coverLetter}
+      </p>
+    </div>
+    <div className="resume">
+      <img
+        src={element.resume.url}
+        alt="resume"
+        onClick={() => openModal(element.resume.url)}
+      />
+    </div>
+  </div>
+);
+
 export default MyApplications;
-
-const JobSeekerCard = ({ element, deleteApplication, openModal }) => {
-  return (
-    <>
-      <div className="job_seeker_card">
-        <div className="detail">
-          <p>
-            <span>Name:</span> {element.name}
-          </p>
-          <p>
-            <span>Email:</span> {element.email}
-          </p>
-          <p>
-            <span>Phone:</span> {element.phone}
-          </p>
-          <p>
-            <span>Address:</span> {element.address}
-          </p>
-          <p>
-            <span>CoverLetter:</span> {element.coverLetter}
-          </p>
-        </div>
-        <div className="resume">
-          <img
-            src={element.resume.url}
-            alt="resume"
-            onClick={() => openModal(element.resume.url)}
-          />
-        </div>
-        <div className="btn_area">
-          <button onClick={() => deleteApplication(element._id)}>
-            Delete Application
-          </button>
-        </div>
-      </div>
-    </>
-  );
-};
-
-const EmployerCard = ({ element, openModal }) => {
-  return (
-    <>
-      <div className="job_seeker_card">
-        <div className="detail">
-          <p>
-            <span>Name:</span> {element.name}
-          </p>
-          <p>
-            <span>Email:</span> {element.email}
-          </p>
-          <p>
-            <span>Phone:</span> {element.phone}
-          </p>
-          <p>
-            <span>Address:</span> {element.address}
-          </p>
-          <p>
-            <span>CoverLetter:</span> {element.coverLetter}
-          </p>
-        </div>
-        <div className="resume">
-          <img
-            src={element.resume.url}
-            alt="resume"
-            onClick={() => openModal(element.resume.url)}
-          />
-        </div>
-      </div>
-    </>
-  );
-};
--- src/components/Job/Jobs.jsx
+++ src/components/Job/Jobs.jsx
@@ -2,59 +2,93 @@ import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../../main";
-// import { Progress } from "@/components/ui/progress";
-import Spinner from "react-bootstrap/Spinner";
+import { RotatingLines } from "react-loader-spinner";
+
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { isAuthorized } = useContext(Context);
-  const [progress, setProgress] = useState(0);
   const navigateTo = useNavigate();
+
   useEffect(() => {
-    try {
-      axios
-        .get("https://jobjet-backend.onrender.com/api/v1/job/getall", {
-          withCredentials: true,
-        })
-        .then((res) => {
-          setJobs(res.data);
-        });
-    } catch (error) {
-      console.log(error);
-    }
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "https://jobjet-backend.onrender.com/api/v1/job/getall",
+          {
+            withCredentials: true,
+          }
+        );
+        setJobs(res.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
-  if (!isAuthorized) {
-    setTimeout(() => {
-      navigateTo("/");
-    }, 5000);
-  }
+
+  useEffect(() => {
+    if (!isAuthorized) {
+      setTimeout(() => {
+        navigateTo("/");
+      }, 5000);
+    }
+  }, [isAuthorized, navigateTo]);
 
   return (
     <section className="jobs page">
       {isAuthorized ? (
         <div className="container">
           <h1>ALL AVAILABLE JOBS</h1>
-          <div className="banner">
-            {jobs.jobs &&
-              jobs.jobs.map((element) => {
-                return (
+          {loading ? (
+            <div
+              className="loading-spinner"
+              style={{
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <RotatingLines
+                strokeColor="grey"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="96"
+                visible={true}
+              />
+            </div>
+          ) : (
+            <div className="banner">
+              {jobs.jobs && jobs.jobs.length > 0 ? (
+                jobs.jobs.map((element) => (
                   <div className="card" key={element._id}>
                     <p>{element.title}</p>
                     <p>{element.category}</p>
                     <p>{element.country}</p>
                     <Link to={`/jobs/${element._id}`}>Job Details</Link>
                   </div>
-                );
-              })}
-          </div>
+                ))
+              ) : (
+                <h4>No Jobs Found</h4>
+              )}
+            </div>
+          )}
         </div>
       ) : (
         <>
           <h1 style={{ width: "full", text: "center" }}>
             Navigating to home as you are not logged in
           </h1>
-          <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="96"
+            visible={true}
+          />
         </>
       )}
     </section>
